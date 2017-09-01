c43669223156d3f6783660a97418b7406d1fbbe4
boctavian96 <boctavian96@yahoo.com>
2017-09-01 12:00:17 +0300

    Solved errors

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nasm -f elf stub.asm -o stub.o
+nasm -f elf64 stub.asm -o stub.o
 
-fpc -Aelf -n -O3 -Op3 -Si -Sc -Sg -Xd -CX -XXs -Rintel -Tlinux kernel.pas
+fpc -Aelf -n -O3 -Op3 -Si -Sc -Sg -Xd -CX -XXs -Rintel -Tlinux kernel.pas 
 
-i386-elf-ld --gc-sections -s -Tlinker.script -o kernel.obj stub.o kernel.o multiboot.o system.o console.o
+ld --gc-sections -s -Tlinker.script -o kernel.obj stub.o kernel.o multiboot.o system.o console.o
