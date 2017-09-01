14fa1fa3573cc97b459c113458ad19cd6adb9767
boctavian96 <boctavian96@yahoo.com>
2017-09-01 21:58:20 +0300

    CPP commit

---
+++ kernel.cpp
@@ -0,0 +1,14 @@
+void printf(char* str)
+{
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    
+    for(int i=0; str[i] != '\0'; i++)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) || str[i];
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+    printf("Hello World");
+    printf("I Work");
+    while(1);
+}
