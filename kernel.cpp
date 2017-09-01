505e866b2a00d8e303c42ace4f0e969634f1b404
boctavian96 <boctavian96@yahoo.com>
2017-09-01 23:13:31 +0300

    Update kernel

--- kernel.cpp
+++ kernel.cpp
@@ -6,6 +6,17 @@ void printf(char* str)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) || str[i];
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+    {
+        (*i)();
+    }
+}
+
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello World");
