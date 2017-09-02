fad9b87709aa6dd8add1e5c7e19b5cf614ef7055
boctavian96 <boctavian96@yahoo.com>
2017-09-02 16:04:31 +0300

    includes

--- kernel.cpp
+++ kernel.cpp
@@ -5,50 +5,8 @@ Author : Octavian
 */
 
 #include "types.h"
-
-void printf(int8_t* str);
-void waitForInput();
-void getInput();
-int getCommand(int8_t* input);
-
-void printf(int8_t* str)
-{
-    uint16_t* VideoMemory = (uint16_t*) 0xb8000;
-
-    for(int i=0; str[i] != '\0'; i++)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-}
-
-void waitForInput()
-{
-    getInput();
-}
-
-void getInput()
-{
-    int8_t* input = "hello";
-    if (1)
-    {
-        printf("FAILURE");
-    }
-    else 
-    {
-        printf("SUCCES");
-    }
-}
-
-int getCommand(int8_t* input)
-{
-    if(1)
-    {
-        printf("Ich liebe Ade");        
-    }
-    else
-    {
-        return 1;
-    }
-    return 0;
-}
+#include "io.h"
+#include "konsole.h"
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
