f1c60df07d6e7c2db32061d04aa492b70b048020
boctavian96 <boctavian96@yahoo.com>
2017-09-02 15:43:36 +0300

    Remake of makefile

--- kernel.cpp
+++ kernel.cpp
@@ -2,16 +2,52 @@
 ***Okton OS Kernel***
 Version : 0.1
 Author : Octavian
-
 */
 
+#include "types.h"
 
-void printf(char* str)
+void printf(int8_t* str);
+void waitForInput();
+void getInput();
+int getCommand(int8_t* input);
+
+void printf(int8_t* str)
 {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
-    
+    uint16_t* VideoMemory = (uint16_t*) 0xb8000;
+
     for(int i=0; str[i] != '\0'; i++)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+void waitForInput()
+{
+    getInput();
+}
+
+void getInput()
+{
+    int8_t* input = "hello";
+    if (1)
+    {
+        printf("FAILURE");
+    }
+    else 
+    {
+        printf("SUCCES");
+    }
+}
+
+int getCommand(int8_t* input)
+{
+    if(1)
+    {
+        printf("Ich liebe Ade");        
+    }
+    else
+    {
+        return 1;
+    }
+    return 0;
 }
 
 typedef void (*constructor)();
@@ -25,7 +61,7 @@ extern "C" void callConstructors()
     }
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int /* magicnumber */)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t /* magicnumber */)
 {
     printf("Okton v0.1, Hello World, WORKS !!!");
     while(1);
---
+++ types.h
@@ -0,0 +1,15 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+    
+    //We want to know how much bytes we use
+
+    typedef char            int8_t;
+    typedef unsigned char   uint8_t;
+    typedef short           int16_t;
+    typedef unsigned short  uint16_t;
+    typedef int             int32_t;
+    typedef unsigned int    uint32_t;
+    typedef long long int   int64_t;
+    typedef unsigned long long int uint64_t;
+
+#endif
