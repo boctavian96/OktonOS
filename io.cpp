2cb994ccb3d0c4fce1728cdce4660db5e020564c
boctavian96 <boctavian96@yahoo.com>
2017-09-04 18:36:27 +0300

    Forgot to commit io

--- io.cpp
+++ io.cpp
@@ -1,7 +1,46 @@
-void printf(char* str)
+#include"types.h"
+
+void printf(uint8_t* str)
 {
-    unsigned short* VideoMemory = (unsigned short*) 0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
+    static uint8_t x = 0;
+    statuc uint8_t y = 0;
 
     for(int i=0; str[i] != '\0'; i++)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    {
+        //VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        switch(str[i])
+        {
+            case '\n':
+                    //If new line then increase y
+                    x = 0; 
+                    y++;
+                break;
+            default:
+                VideoMemory[80*y + x] = (VideoMemory[80*y + x] & 0xFF00) | str[i];
+                x++;
+                break;
+        } 
+        
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+        
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+            {
+                for(x = 0; x < 80; x++)
+                {
+                    VideoMemory[80*y + x] = (VideoMemory[80*y + x] & 0xFF00) | ' ';
+                }
+                
+                x = 0;
+                y = 0;
+            }
+        }
+        
+    }
 }
