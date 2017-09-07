e0c056df912c780cdc2fd8de5f8600fb0699b9c5
boctavian96 <boctavian96@yahoo.com>
2017-09-07 22:40:06 +0300

    test

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@
 #![no_std]
 
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn rust_main() {
+    let x = ["Hello", "Pisi", "!"];
+    let y = x;
+}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> !{loop{}}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
