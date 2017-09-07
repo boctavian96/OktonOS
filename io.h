a55d1bb6aa092a3bca59630bdf315d00ee8ca8e3
boctavian96 <boctavian96@yahoo.com>
2017-09-07 21:17:22 +0300

    removing

--- io.h
+++
@@ -1,6 +0,0 @@
-#ifndef __IO_H
-#define __IO_H
-
-void printf(int8_t* str);
-
-#endif
--- konsole.h
+++
@@ -1,8 +0,0 @@
-#ifndef __KONSOLE_H
-#define __KONSOLE_H
-
-void waitForInput();
-void getInput();
-int getCommand(char* input);
-
-#endif
