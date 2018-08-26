82b25578dbcf9aec403e344fa14ad98ddc6507ae
kivimango <balazsviktor23@gmail.com>
2018-08-26 21:44:01 +0200

    Added overloaded operators for arrays

--- include/memorymanagement.h
+++ include/memorymanagement.h
@@ -2,29 +2,27 @@
 #ifndef __MYOS__MEMORYMANAGEMENT_H
 #define __MYOS__MEMORYMANAGEMENT_H
 
-#include <common/types.h>
 
+#include<common/types.h>
 
 namespace myos
 {
     
     struct MemoryChunk
     {
-        MemoryChunk *next;
-        MemoryChunk *prev;
         bool allocated;
+        MemoryChunk* next;
+        MemoryChunk* prev;
         common::size_t size;
     };
     
     
     class MemoryManager
     {
-        
     protected:
-        MemoryChunk* first;
+        MemoryChunk* firstchunk;
     public:
-        
-        static MemoryManager *activeMemoryManager;
+        static MemoryManager* activeMemoryManager;
         
         MemoryManager(common::size_t first, common::size_t size);
         ~MemoryManager();
@@ -32,18 +30,17 @@ namespace myos
         void* malloc(common::size_t size);
         void free(void* ptr);
     };
+    
 }
 
 
 void* operator new(unsigned size);
 void* operator new[](unsigned size);
-
 // placement new
-void* operator new(unsigned size, void* ptr);
-void* operator new[](unsigned size, void* ptr);
+void* operator new(myos::common::size_t size, void *ptr);
+void* operator new[](myos::common::size_t size, void *ptr);
 
 void operator delete(void* ptr);
 void operator delete[](void* ptr);
-
 
 #endif
